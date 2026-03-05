6a441c7a5fdb067ba351ec839f99281f248311c6
Kota Kato <45391880+kat0h@users.noreply.github.com>
2026-03-05 21:01:20 +0900

    void server

--- esp32-c3-ir-server/src/main.cpp
+++ esp32-c3-ir-server/src/main.cpp
@@ -1,9 +1,37 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#define NEOPIXEL 4
 
-void setup(){
-  neopixelWrite(NEOPIXEL,8,8,8);
+#include <FastLED.h>
+#define NEOPIXEL 4
+#define NUM_LEDS 3
+CRGB leds[NUM_LEDS];
+
+#include <ESPmDNS.h>
+#include <WiFi.h>
+#include <WebServer.h>
+WebServer server(80);
+
+void setup_server() {
+  server.begin();
 }
-void loop(){
+
+void setup() {
+  FastLED.addLeds<WS2812, NEOPIXEL>(leds, NUM_LEDS);
+  Serial.begin(250000);
+
+  WiFi.begin();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.println("Connecting to Wifi...");
+  }
+  MDNS.begin("esp32");
+  Serial.println("esp32.local");
+  setup_server();
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Wi-Fi disconnected. Restarting.");
+    ESP.restart();
+  }
+  server.handleClient();
 }
