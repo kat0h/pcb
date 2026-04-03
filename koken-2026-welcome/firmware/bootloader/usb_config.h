6aecae313c933a5926fcd78bc31450bb55224d40
Kota Kato <45391880+kat0h@users.noreply.github.com>
2026-04-03 17:20:57 +0900

    bootloaderの動作確認
    configurebootloaderを使うことで、書き込んだブートローダーを利用できるようになった

--- koken-2026-welcome/firmware/bootloader/usb_config.h
+++ koken-2026-welcome/firmware/bootloader/usb_config.h
@@ -19,7 +19,8 @@
 #define USB_PORT C     // [A,C,D] GPIO Port to use with D+, D- and DPU
 #define USB_PIN_DP 4   // [0-4] GPIO Number for USB D+ Pin
 #define USB_PIN_DM 2   // [0-4] GPIO Number for USB D- Pin
-// #define USB_PIN_DPU 1  // [0-7] GPIO for feeding the 1.5k Pull-Up on USB D- Pin; Comment out if not used / tied to 3V3!
+#define USB_PORT_DPU C  // [A,C,D] Override GPIO Port for DPU
+#define USB_PIN_DPU 1  // [0-7] GPIO for feeding the 1.5k Pull-Up on USB D- Pin; Comment out if not used / tied to 3V3!
 
 #define RV003USB_OPTIMIZE_FLASH 1
 
