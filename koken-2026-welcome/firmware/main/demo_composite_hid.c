8affbe356c9ab2849700a12da23a1ab50b9d6002
Kota Kato <45391880+kat0h@users.noreply.github.com>
2026-04-05 13:15:38 +0900

    バッテリー駆動時のコードを記述
    ボタンを押すと明るく光る

--- koken-2026-welcome/firmware/main/demo_composite_hid.c
+++ koken-2026-welcome/firmware/main/demo_composite_hid.c
@@ -10,42 +10,28 @@
 
 #define BT_PIN GPIOv_from_PORT_PIN(GPIO_port_A, 1)
 #define NP_PIN 2 // PA2
-uint8_t before_btn_state;
 
-int main() {
-  SystemInit();
-  funGpioInitAll();
-  GPIO_port_enable(GPIO_port_A); // initialize neopixel
-  WS2812BSimpleSend(GPIOA, NP_PIN, (uint8_t[]){0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 6);
-  GPIO_pinMode(BT_PIN, GPIO_pinMode_I_pullUp, GPIO_Speed_In); // initialize button
-  adc_init_vref();
-  uint8_t usb_connected = get_vcc_mv() > 3200; // power supply is usb?
-  if (usb_connected) usb_setup();
-  before_btn_state = GPIO_digitalRead(BT_PIN);
+void battery_main() {
   uint8_t wheel_pos = 0;
-  uint8_t brightness = usb_connected ? 50 : 5;
   uint8_t all_led_data[9] = {0};
   while (1) {
-    uint8_t current_btn_state = GPIO_digitalRead(BT_PIN);
+    uint8_t brightness = !GPIO_digitalRead(BT_PIN) ? 50 : 5;
     wheel_pos += 4;
-    if (current_btn_state != usb_connected) {
-      for (int i = 0; i < 3; i++) {
-        // 各LEDの色相をずらす
-        uint8_t hue = wheel_pos + (i * (255 / 4));
-        uint32_t color = EHSVtoHEX(hue, 255, brightness);
-        all_led_data[i * 3 + 0] = (color >> 8) & 0xFF;
-        all_led_data[i * 3 + 1] = (color >> 0) & 0xFF;
-        all_led_data[i * 3 + 2] = (color >> 16) & 0xFF;
-      }
-      WS2812BSimpleSend(GPIOA, NP_PIN, all_led_data, 9);
-    } else {
-      WS2812BSimpleSend(
-          GPIOA, NP_PIN,
-          (uint8_t[]){0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 9);
+    for (int i = 0; i < 3; i++) {
+      // 各LEDの色相をずらす
+      uint8_t hue = wheel_pos + (i * (255 / 4));
+      uint32_t color = EHSVtoHEX(hue, 255, brightness);
+      all_led_data[i * 3 + 0] = (color >> 8) & 0xFF;
+      all_led_data[i * 3 + 1] = (color >> 0) & 0xFF;
+      all_led_data[i * 3 + 2] = (color >> 16) & 0xFF;
     }
+    WS2812BSimpleSend(GPIOA, NP_PIN, all_led_data, 9);
     Delay_Ms(30);
-    before_btn_state = current_btn_state;
   }
+}
+
+void keyboard_main() {
+  usb_setup();
 }
 
 void usb_handle_user_data(struct usb_endpoint *e, int current_endpoint,
@@ -55,4 +41,16 @@ void usb_handle_user_in_request(struct usb_endpoint *e, uint8_t *scratchpad,
                                 int endp, uint32_t sendtok,
                                 struct rv003usb_internal *ist) {
   usb_send_empty(sendtok);
+}
+
+int main() {
+  SystemInit();
+  funGpioInitAll();
+  GPIO_port_enable(GPIO_port_A); // initialize neopixel
+  WS2812BSimpleSend(GPIOA, NP_PIN, (uint8_t[]){0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 6);
+  GPIO_pinMode(BT_PIN, GPIO_pinMode_I_pullUp, GPIO_Speed_In); // initialize button
+  adc_init_vref();
+  uint8_t usb_connected = get_vcc_mv() > 3200; // power supply is usb?
+  if (usb_connected) keyboard_main();
+  else battery_main();
 }
