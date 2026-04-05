d5ba121ed1435a41bdb991bc5c04bcb3d14752f2
Kota Kato <45391880+kat0h@users.noreply.github.com>
2026-04-05 14:28:56 +0900

    単押しと長押しを区別

--- koken-2026-welcome/firmware/main/keyboard.c
+++ koken-2026-welcome/firmware/main/keyboard.c
@@ -1,14 +1,39 @@
-uint8_t state = 0;
-uint8_t blue[6]  = {0, 0, 255, 0, 0, 255};
-uint8_t red[6]   = {0, 255, 0, 0, 255, 0};
+uint8_t blue[6]  = {0, 0, 25, 0, 0, 25};
+uint8_t red[6]   = {0, 25, 0, 0, 25, 0};
 uint8_t black[6] = {0, 0, 0, 0, 0, 0};
 
+uint8_t state = 0;
+uint8_t count = 0;
 void keyboard_main() {
   uint8_t v; // button state
   while (1) {
     v = !GPIO_digitalRead(BT_PIN);
-    if (v) WS2812BSimpleSend(GPIOA, NP_PIN, blue, 6);
-    else WS2812BSimpleSend(GPIOA, NP_PIN, black, 6);
+    switch (state) {
+      case 0:
+        if (v) {
+          state = 1;
+          count = 0;
+        }
+        break;
+      case 1:
+        if (v) {
+          count += 10;
+        } else {
+          state = 0;
+          // dot
+          WS2812BSimpleSend(GPIOA, NP_PIN, blue, 6);
+        }
+        if (count > 150)
+          state = 2;
+        break;
+      case 2:
+        if (!v) {
+          state = 0;
+          // dash
+          WS2812BSimpleSend(GPIOA, NP_PIN, red, 6);
+        }
+        break;
+    }
     Delay_Ms(10);
   }
 }
