be78abc65ccccc12dc5936cabfb35e191b2c7ed2
Kota Kato <45391880+kat0h@users.noreply.github.com>
2026-04-05 13:37:48 +0900

    delay_msの代わりにsleepするように

--- koken-2026-welcome/firmware/main/demo_composite_hid.c
+++ koken-2026-welcome/firmware/main/demo_composite_hid.c
@@ -11,7 +11,47 @@
 #define BT_PIN GPIOv_from_PORT_PIN(GPIO_port_A, 1)
 #define NP_PIN 2 // PA2
 
+// 1. AWU用の割り込みハンドラを定義する (これがないと復帰時にフリーズします)
+// __attribute__((interrupt)) はRISC-Vの割り込み処理に必須です
+void AWU_IRQHandler(void) __attribute__((interrupt));
+void AWU_IRQHandler(void) {
+    // EXTI Line 9 (AWU) の割り込みフラグをクリア
+    EXTI->INTFR = (1 << 9);
+}
+
+void awu_init_30ms(void) {
+    // PWRモジュールのクロックとLSI(128kHz)を有効化
+    RCC->APB1PCENR |= RCC_APB1Periph_PWR;
+    RCC->RSTSCKR |= RCC_LSION;
+    while ((RCC->RSTSCKR & RCC_LSIRDY) == 0);
+    // 2. イベント(EVENR)ではなく【割り込み(INTENR)】を有効化
+    EXTI->INTENR |= (1 << 9);
+    // 3. AWUの仕様に合わせて【立ち下がりエッジ(FTENR)】に変更
+    EXTI->FTENR |= (1 << 9);
+    // PFIC (割り込みコントローラ) で AWU 割り込みを許可 (AWU_IRQn = 4)
+    NVIC_EnableIRQ(AWU_IRQn);
+    // 30msの設定: (59+1)*64 / 128000 = 0.03秒
+    PWR->AWUPSC = 7;
+    PWR->AWUWR  = 59;
+    // AWUを有効化
+    PWR->AWUCSR |= (1 << 1);
+}
+
+void enter_stop_mode_30ms(void) {
+    // スリープに入る前に念のためフラグをクリア
+    EXTI->INTFR = (1 << 9);
+    // ストップモードを選択
+    PWR->CTLR &= ~PWR_CTLR_PDDS;
+    PFIC->SCTLR |= (1 << 2);
+    // WFI (Wait For Interrupt) 命令でスリープに移行
+    __asm__ volatile ("wfi");
+    // --- 30ms経過後、AWU_IRQHandler が実行され、ここに戻ってくる ---
+    // SLEEPDEEPを解除して通常状態に戻す
+    PFIC->SCTLR &= ~(1 << 2);
+}
+
 void battery_main() {
+  awu_init_30ms();
   uint8_t wheel_pos = 0;
   uint8_t all_led_data[9] = {0};
   while (1) {
@@ -26,12 +66,12 @@ void battery_main() {
       all_led_data[i * 3 + 2] = (color >> 16) & 0xFF;
     }
     WS2812BSimpleSend(GPIOA, NP_PIN, all_led_data, 9);
-    Delay_Ms(30);
+    enter_stop_mode_30ms();
   }
 }
 
 void keyboard_main() {
-  usb_setup();
+  battery_main();
 }
 
 void usb_handle_user_data(struct usb_endpoint *e, int current_endpoint,
